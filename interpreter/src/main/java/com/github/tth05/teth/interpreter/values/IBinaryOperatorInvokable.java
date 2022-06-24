ad480dc1e844c73cf1feaea415af302cb3b19756
tth05 <36999320+tth05@users.noreply.github.com>
2022-06-24 15:00:12 +0200

    Remote AST interpreter

--- interpreter/src/main/java/com/github/tth05/teth/interpreter/values/IBinaryOperatorInvokable.java
+++
@@ -1,9 +0,0 @@
-package com.github.tth05.teth.interpreter.values;
-
-import com.github.tth05.teth.lang.parser.ast.BinaryExpression;
-
-public interface IBinaryOperatorInvokable {
-
-
-    IValue invokeBinaryOperator(BinaryExpression.Operator operator, IValue arg);
-}
--- interpreter/src/main/java/com/github/tth05/teth/interpreter/values/IUnaryOperatorInvokable.java
+++
@@ -1,9 +0,0 @@
-package com.github.tth05.teth.interpreter.values;
-
-import com.github.tth05.teth.interpreter.values.IValue;
-import com.github.tth05.teth.lang.parser.ast.UnaryExpression;
-
-public interface IUnaryOperatorInvokable {
-
-    IValue invokeUnaryOperator(UnaryExpression.Operator operator);
-}
