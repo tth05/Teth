3ea47f16b6acd3e99a71f2f7d172642c633b9efb
tth05 <36999320+tth05@users.noreply.github.com>
2022-10-18 12:22:37 +0200

    Intellij: Use new auto-completion API

--- intellij-plugin/src/main/kotlin/com/github/tth05/tethintellijplugin/completion/TethCompletionContributor.kt
+++ intellij-plugin/src/main/kotlin/com/github/tth05/tethintellijplugin/completion/TethCompletionContributor.kt
@@ -1,11 +1,16 @@
 package com.github.tth05.tethintellijplugin.completion
 
+import com.github.tth05.teth.analyzer.completion.AutoCompletion
+import com.github.tth05.tethintellijplugin.psi.caching.tethCache
+import com.github.tth05.tethintellijplugin.syntax.analyzeAndParseFile
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.icons.AllIcons
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.psi.ElementManipulators
 import com.intellij.psi.PsiElement
 import com.intellij.util.ProcessingContext
+import com.jetbrains.rd.util.string.printToString
 
 class TethCompletionContributor : CompletionContributor() {
 
@@ -16,13 +21,13 @@ class TethCompletionContributor : CompletionContributor() {
                 override fun addCompletions(
                     parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet
                 ) {
-                    val originalElement = CompletionUtil.getOriginalOrSelf<PsiElement>(parameters.position)
-                    val actualText = ElementManipulators.getValueText(originalElement)
-                    if (actualText.isBlank()) return
+                    val (analyzer, _, parserResult) = parameters.originalFile.tethCache()
+                        .resolveWithCaching(parameters.originalFile) { analyzeAndParseFile(it) }
 
-                    result.addElement(LookupElementBuilder.create(actualText + Math.random()))
-                    result.addElement(LookupElementBuilder.create(actualText + Math.random()))
-                    result.addElement(LookupElementBuilder.create(actualText + Math.random()))
+                    val autoCompletion = AutoCompletion(analyzer, parserResult.unit)
+                    autoCompletion.complete(parameters.offset).forEach {
+                        result.addElement(LookupElementBuilder.create(it.text).withIcon(AllIcons.Nodes.Method))
+                    }
                 }
             })
     }
