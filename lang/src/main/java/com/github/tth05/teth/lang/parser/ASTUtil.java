93e20fa23559b84c03e465a1ee7eeca03d999fcf
tth05 <36999320+tth05@users.noreply.github.com>
2022-10-18 12:21:28 +0200

    Lang: Add endInclusive parameter to ASTUtil#walkNodesAtOffset

--- lang/src/main/java/com/github/tth05/teth/lang/parser/ASTUtil.java
+++ lang/src/main/java/com/github/tth05/teth/lang/parser/ASTUtil.java
@@ -27,13 +27,13 @@ public class ASTUtil {
         var collector = new NodeCollector();
 
         for (var statement : unit.getStatements()) {
-            walkNodesAtOffset(statement, offset, collector);
+            walkNodesAtOffset(statement, offset, false, collector);
         }
 
         return collector.statement;
     }
 
-    public static void walkNodesAtOffset(Statement statement, int offset, Consumer<Statement> consumer) {
+    public static void walkNodesAtOffset(Statement statement, int offset, boolean endInclusive, Consumer<Statement> consumer) {
         if (statement == null)
             return;
 
@@ -41,85 +41,85 @@ public class ASTUtil {
         if (span == null)
             return;
 
-        if (offset < span.offset() || offset >= span.offsetEnd())
+        if (offset < span.offset() || offset > span.offsetEnd() || (!endInclusive && offset == span.offsetEnd()))
             return;
 
         consumer.accept(statement);
 
         if (statement instanceof BinaryExpression expr) {
-            walkNodesAtOffset(expr.getLeft(), offset, consumer);
-            walkNodesAtOffset(expr.getRight(), offset, consumer);
+            walkNodesAtOffset(expr.getLeft(), offset, endInclusive, consumer);
+            walkNodesAtOffset(expr.getRight(), offset, endInclusive, consumer);
         } else if (statement instanceof BlockStatement block) {
-            walkNodesAtOffset(block.getStatements(), offset, consumer);
+            walkNodesAtOffset(block.getStatements(), offset, endInclusive, consumer);
         } else if (statement instanceof GenericParameterDeclaration p) {
-            walkNodesAtOffset(p.getNameExpr(), offset, consumer);
+            walkNodesAtOffset(p.getNameExpr(), offset, endInclusive, consumer);
         } else if (statement instanceof FunctionDeclaration func) {
-            walkNodesAtOffset(func.getNameExpr(), offset, consumer);
-            walkNodesAtOffset(func.getGenericParameters(), offset, consumer);
-            walkNodesAtOffset(func.getParameters(), offset, consumer);
-            walkNodesAtOffset(func.getReturnTypeExpr(), offset, consumer);
-            walkNodesAtOffset(func.getBody(), offset, consumer);
+            walkNodesAtOffset(func.getNameExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(func.getGenericParameters(), offset, endInclusive, consumer);
+            walkNodesAtOffset(func.getParameters(), offset, endInclusive, consumer);
+            walkNodesAtOffset(func.getReturnTypeExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(func.getBody(), offset, endInclusive, consumer);
         } else if (statement instanceof FunctionDeclaration.ParameterDeclaration param) {
-            walkNodesAtOffset(param.getNameExpr(), offset, consumer);
-            walkNodesAtOffset(param.getTypeExpr(), offset, consumer);
+            walkNodesAtOffset(param.getNameExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(param.getTypeExpr(), offset, endInclusive, consumer);
         } else if (statement instanceof FunctionInvocationExpression expr) {
-            walkNodesAtOffset(expr.getTarget(), offset, consumer);
-            walkNodesAtOffset(expr.getGenericParameters(), offset, consumer);
-            walkNodesAtOffset(expr.getParameters(), offset, consumer);
+            walkNodesAtOffset(expr.getTarget(), offset, endInclusive, consumer);
+            walkNodesAtOffset(expr.getGenericParameters(), offset, endInclusive, consumer);
+            walkNodesAtOffset(expr.getParameters(), offset, endInclusive, consumer);
         } else if (statement instanceof IfStatement ifStmt) {
-            walkNodesAtOffset(ifStmt.getCondition(), offset, consumer);
-            walkNodesAtOffset(ifStmt.getBody(), offset, consumer);
-            walkNodesAtOffset(ifStmt.getElseStatement(), offset, consumer);
+            walkNodesAtOffset(ifStmt.getCondition(), offset, endInclusive, consumer);
+            walkNodesAtOffset(ifStmt.getBody(), offset, endInclusive, consumer);
+            walkNodesAtOffset(ifStmt.getElseStatement(), offset, endInclusive, consumer);
         } else if (statement instanceof ListLiteralExpression expr) {
-            walkNodesAtOffset(expr.getInitializers(), offset, consumer);
+            walkNodesAtOffset(expr.getInitializers(), offset, endInclusive, consumer);
         } else if (statement instanceof LoopStatement loop) {
-            walkNodesAtOffset(loop.getVariableDeclarations(), offset, consumer);
-            walkNodesAtOffset(loop.getCondition(), offset, consumer);
-            walkNodesAtOffset(loop.getAdvanceStatement(), offset, consumer);
-            walkNodesAtOffset(loop.getBody(), offset, consumer);
+            walkNodesAtOffset(loop.getVariableDeclarations(), offset, endInclusive, consumer);
+            walkNodesAtOffset(loop.getCondition(), offset, endInclusive, consumer);
+            walkNodesAtOffset(loop.getAdvanceStatement(), offset, endInclusive, consumer);
+            walkNodesAtOffset(loop.getBody(), offset, endInclusive, consumer);
         } else if (statement instanceof MemberAccessExpression expr) {
-            walkNodesAtOffset(expr.getTarget(), offset, consumer);
-            walkNodesAtOffset(expr.getMemberNameExpr(), offset, consumer);
+            walkNodesAtOffset(expr.getTarget(), offset, endInclusive, consumer);
+            walkNodesAtOffset(expr.getMemberNameExpr(), offset, endInclusive, consumer);
         } else if (statement instanceof ObjectCreationExpression expr) {
-            walkNodesAtOffset(expr.getTargetNameExpr(), offset, consumer);
-            walkNodesAtOffset(expr.getGenericParameters(), offset, consumer);
-            walkNodesAtOffset(expr.getParameters(), offset, consumer);
+            walkNodesAtOffset(expr.getTargetNameExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(expr.getGenericParameters(), offset, endInclusive, consumer);
+            walkNodesAtOffset(expr.getParameters(), offset, endInclusive, consumer);
         } else if (statement instanceof ParenthesisedExpression expr) {
-            walkNodesAtOffset(expr.getExpression(), offset, consumer);
+            walkNodesAtOffset(expr.getExpression(), offset, endInclusive, consumer);
         } else if (statement instanceof ReturnStatement ret) {
-            walkNodesAtOffset(ret.getValueExpr(), offset, consumer);
+            walkNodesAtOffset(ret.getValueExpr(), offset, endInclusive, consumer);
         } else if (statement instanceof StructDeclaration struct) {
-            walkNodesAtOffset(struct.getNameExpr(), offset, consumer);
-            walkNodesAtOffset(struct.getGenericParameters(), offset, consumer);
-            walkNodesAtOffset(struct.getFields(), offset, consumer);
-            walkNodesAtOffset(struct.getFunctions(), offset, consumer);
+            walkNodesAtOffset(struct.getNameExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(struct.getGenericParameters(), offset, endInclusive, consumer);
+            walkNodesAtOffset(struct.getFields(), offset, endInclusive, consumer);
+            walkNodesAtOffset(struct.getFunctions(), offset, endInclusive, consumer);
         } else if (statement instanceof StructDeclaration.FieldDeclaration field) {
-            walkNodesAtOffset(field.getNameExpr(), offset, consumer);
-            walkNodesAtOffset(field.getTypeExpr(), offset, consumer);
+            walkNodesAtOffset(field.getNameExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(field.getTypeExpr(), offset, endInclusive, consumer);
         } else if (statement instanceof TypeExpression expr) {
-            walkNodesAtOffset(expr.getNameExpr(), offset, consumer);
-            walkNodesAtOffset(expr.getGenericParameters(), offset, consumer);
+            walkNodesAtOffset(expr.getNameExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(expr.getGenericParameters(), offset, endInclusive, consumer);
         } else if (statement instanceof UnaryExpression expr) {
-            walkNodesAtOffset(expr.getExpression(), offset, consumer);
+            walkNodesAtOffset(expr.getExpression(), offset, endInclusive, consumer);
         } else if (statement instanceof UseStatement use) {
-            walkNodesAtOffset(use.getPathExpr(), offset, consumer);
-            walkNodesAtOffset(use.getImports(), offset, consumer);
+            walkNodesAtOffset(use.getPathExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(use.getImports(), offset, endInclusive, consumer);
         } else if (statement instanceof VariableDeclaration var) {
-            walkNodesAtOffset(var.getNameExpr(), offset, consumer);
-            walkNodesAtOffset(var.getTypeExpr(), offset, consumer);
-            walkNodesAtOffset(var.getInitializerExpr(), offset, consumer);
+            walkNodesAtOffset(var.getNameExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(var.getTypeExpr(), offset, endInclusive, consumer);
+            walkNodesAtOffset(var.getInitializerExpr(), offset, endInclusive, consumer);
         } else if (statement instanceof StringLiteralExpression expr) {
             for (StringLiteralExpression.Part part : expr.getParts()) {
                 if (part.getType() != StringLiteralExpression.PartType.EXPRESSION)
                     continue;
 
-                walkNodesAtOffset(part.asExpression(), offset, consumer);
+                walkNodesAtOffset(part.asExpression(), offset, endInclusive, consumer);
             }
         }
     }
 
-    private static void walkNodesAtOffset(List<? extends Statement> children, int offset, Consumer<Statement> consumer) {
+    public static void walkNodesAtOffset(List<? extends Statement> children, int offset, boolean endInclusive, Consumer<Statement> consumer) {
         for (var child : children)
-            walkNodesAtOffset(child, offset, consumer);
+            walkNodesAtOffset(child, offset, endInclusive, consumer);
     }
 }
